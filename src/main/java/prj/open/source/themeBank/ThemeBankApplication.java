3954ac5b66f460ef36caad1f7cdff92ea00aeaf2
zhaodanyang <zhaodanyang@zhijin.com>
2018-04-03 20:36:54 +0800

    spring boot

---
+++ src/main/java/prj/open/source/themeBank/ThemeBankApplication.java
@@ -0,0 +1,12 @@
+package prj.open.source.themeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeBankApplication.class, args);
+	}
+}
---
+++ src/test/java/prj/open/source/themeBank/ThemeBankApplicationTests.java
@@ -0,0 +1,16 @@
+package prj.open.source.themeBank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThemeBankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
